104eb2ac99756a1ec8d79bb477f3fa2acf89b188
Andy Likuski <andy@likuski.org>
2018-01-29 15:52:38 -0800

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-const R = require('ramda');
+const {v} = require('./functionValidator')
+const {vMergeScope} = require('./scopeValidator')
+const {validatePropType, vPropOfFunction, vProps} = require('./propTypesValidator')
 
-module.exports = R.mergeAll([
-  require('./functionValidator'),
-  require('./scopeValidator'),
-  require('./propTypesValidator')
-]);
+module.exports = {
+  v, vMergeScope, validatePropType, vPropOfFunction, vProps
+}
