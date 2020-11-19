446672afe465816c0f07aba4e8f8337de738434e
Andy Likuski <andy@likuski.org>
2020-11-19 18:17:05 +0100

    Added .js to local imports

--- src/index.mjs
+++ src/index.mjs
@@ -9,9 +9,9 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import {v} from './functionValidator';
-import {vMergeScope} from './scopeValidator';
-import {validatePropType, vPropOfFunction, vProps} from './propTypesValidator';
+import {v} from './functionValidator.js';
+import {vMergeScope} from './scopeValidator.js';
+import {validatePropType, vPropOfFunction, vProps} from './propTypesValidator.js';
 
 export {
   v, vMergeScope, validatePropType, vPropOfFunction, vProps
